4cab0f32215270ab0245fc15590141bbdddb6063
Luis David Salazar <luidasa@gmail.com>
2021-05-17 15:24:39 -0500

    Se agrega la entidad de Notificacion y de Role

---
+++ database/migrations/2021_05_17_181823_create_condominios_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCondominiosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('condominios', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('nombre');
+            $table->string('domicilio');
+            $table->string('codigo_postal');
+            $table->string('estado');
+            $table->string('logo');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('condominios');
+    }
+}
